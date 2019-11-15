8b15dd32dca33c15d420096fe35f0d4fecbbe62d
pmayur <thisispatil@gmail.com>
2019-11-15 16:59:11 +0530

    Add Products Display on Home Screen

--- lib/navigation/productListTab.dart
+++ lib/navigation/productListTab.dart
@@ -2,18 +2,46 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
 import '../models/appStateModel.dart';
+import '../productRowItems.dart';
 
 class ProductListTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(
-      builder: (context, model, child) {
-        return CustomScrollView(
 
-          slivers: const <Widget>[
+      builder: (context, model, child) {
+        final products = model.getProducts();
+
+        return CustomScrollView(
+          semanticChildCount: products.length,
+
+          slivers: <Widget>[
+
             CupertinoSliverNavigationBar(
               largeTitle: Text('Cupertino Store'),
             ),
+
+            SliverSafeArea(
+              top: false,
+              minimum: const EdgeInsets.only(top: 8),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+
+                  (context, index) {
+                    if (index < products.length) {
+                      return ProductRowItem(
+                        index: index,
+                        product: products[index],
+                        lastItem: index == products.length - 1,
+                      );
+                    }
+                    return null;
+                  },
+
+                ),
+              ),
+            )
+
           ],
 
         );
