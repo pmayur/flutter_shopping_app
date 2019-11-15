29107172a73b3dbbdae4cb1a94fdbdc209208319
pmayur <thisispatil@gmail.com>
2019-11-15 15:40:57 +0530

    Add Base Navigation

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,17 +1,61 @@
 import 'package:flutter/cupertino.dart';
 // import 'styles/styles.dart';
+import 'package:shopping_app/navigation/productListTab.dart';
+import 'package:shopping_app/navigation/searchTab.dart';
+import 'package:shopping_app/navigation/shoppingCartTab.dart';
 
 class CupertinoStoreHomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
 
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: const Text('Cupertino Store'),
+      tabBar: CupertinoTabBar(
+
+        items: const <BottomNavigationBarItem>[
+
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            title: Text('Products'),
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            title: Text('Search'),
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            title: Text('Cart'),
+          ),
+
+        ],
       ),
-      child: Container(),
-    );
 
+      tabBuilder: (context, index) {
+        switch (index) {
+
+          case 0:
+            return CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: ProductListTab(),
+              );
+            });
+
+          case 1:
+            return CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: SearchTab(),
+              );
+            });
+
+          case 2:
+            return CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: ShoppingCartTab(),
+              );
+            });
+        }
+      },
+    );
   }
 }
