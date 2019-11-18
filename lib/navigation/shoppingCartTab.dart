849823b693b49a0d46c755825d9ea9345f252851
pmayur <thisispatil@gmail.com>
2019-11-18 12:57:31 +0530

    Add Show Cart Items Function

--- lib/navigation/shoppingCartTab.dart
+++ lib/navigation/shoppingCartTab.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 
 import '../models/appStateModel.dart';
+import '../models/product.dart';
 import '../styles/styles.dart';
 
 
@@ -22,6 +23,7 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
   String location;
   String pin;
   DateTime dateTime = DateTime.now();
+  final _currencyFormat = NumberFormat.currency(symbol: '\$');
 
   Widget _buildNameField() {
     return CupertinoTextField(
@@ -140,11 +142,13 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
   }
 
 
-  SliverChildBuilderDelegate _buildSliverChildBuilderDelegate(
-      AppStateModel model) {
-        
+  SliverChildBuilderDelegate _buildSliverChildBuilderDelegate(AppStateModel model) {
+
     return SliverChildBuilderDelegate(
       (context, index) {
+
+        final productIndex = index - 4;
+
         switch (index) {
           case 0:
             return Padding(
@@ -167,7 +171,47 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
               child: _buildDateAndTimePicker(context),
             );
           default:
-          // Do nothing. For now.
+
+            if (model.productsInCart.length > productIndex) {
+              return ShoppingCartItem(
+                index: index,
+                product: model.getProductById(
+                    model.productsInCart.keys.toList()[productIndex]),
+                quantity: model.productsInCart.values.toList()[productIndex],
+                lastItem: productIndex == model.productsInCart.length - 1,
+                formatter: _currencyFormat,
+              );
+            } else if (model.productsInCart.keys.length == productIndex &&
+                model.productsInCart.isNotEmpty) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          'Shipping '
+                              '${_currencyFormat.format(model.shippingCost)}',
+                          style: Styles.productRowItemPrice,
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          'Tax ${_currencyFormat.format(model.tax)}',
+                          style: Styles.productRowItemPrice,
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          'Total  ${_currencyFormat.format(model.totalCost)}',
+                          style: Styles.productRowTotal,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }
         }
         return null;
       },
@@ -179,6 +223,7 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
     return Consumer<AppStateModel>(
       builder: (context, model, child) {
         return CustomScrollView(
+
           slivers: <Widget>[
             const CupertinoSliverNavigationBar(
               largeTitle: Text('Shopping Cart'),
@@ -191,8 +236,90 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
               ),
             )
           ],
+
         );
       },
     );
+  }
+}
+
+
+class ShoppingCartItem extends StatelessWidget {
+  const ShoppingCartItem({
+    @required this.index,
+    @required this.product,
+    @required this.lastItem,
+    @required this.quantity,
+    @required this.formatter,
+  });
+
+  final Product product;
+  final int index;
+  final bool lastItem;
+  final int quantity;
+  final NumberFormat formatter;
+
+  @override
+  Widget build(BuildContext context) {
+    final row = SafeArea(
+      top: false,
+      bottom: false,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          top: 8,
+          bottom: 8,
+          right: 8,
+        ),
+        child: Row(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.cover,
+                width: 40,
+                height: 40,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          product.name,
+                          style: Styles.productRowItemName,
+                        ),
+                        Text(
+                          '${formatter.format(quantity * product.price)}',
+                          style: Styles.productRowItemName,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      '${quantity > 1 ? '$quantity x ' : ''}'
+                      '${formatter.format(product.price)}',
+                      style: Styles.productRowItemPrice,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return row;
   }
 }
